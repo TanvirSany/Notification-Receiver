5b8eb327ecc7345dd68f71d4284864bf96a1f426
TanvirSany <tanvirrahmansany@gmail.com>
2024-10-06 12:53:02 +0600

    Add action to notification

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.notification
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
@@ -51,6 +53,60 @@ class MainActivity : AppCompatActivity() {
 
     fun startNotification(){
 
+        val intent = Intent(applicationContext,MainActivity::class.java)
+
+        val pendingIntent = if(Build.VERSION.SDK_INT>=23) {
+            PendingIntent.getActivity(
+                applicationContext,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }else {
+            PendingIntent.getActivity(
+                applicationContext,
+                0,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        //actionButton
+        val actionIntent = Intent(applicationContext,Receiver::class.java)
+        actionIntent.putExtra("toast", "This is notification Message")
+
+        val actionPending = if(Build.VERSION.SDK_INT>=23) {
+            PendingIntent.getBroadcast(
+                applicationContext,
+                1,
+                actionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }else {
+            PendingIntent.getBroadcast(
+                applicationContext,
+                1,
+                actionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        //dismissButton
+        val dismissIntent = Intent(applicationContext,ReceiverDismiss::class.java)
+
+        val dismissPending = if(Build.VERSION.SDK_INT>=23) {
+            PendingIntent.getBroadcast(
+                applicationContext,
+                2,
+                dismissIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }else {
+            PendingIntent.getBroadcast(
+                applicationContext,
+                2,
+                dismissIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
         val builder = NotificationCompat.Builder(this@MainActivity,CHANNEL_ID,)
 
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
@@ -60,11 +116,20 @@ class MainActivity : AppCompatActivity() {
             builder.setSmallIcon(R.drawable.small_icon)
                 .setContentTitle("Title")
                 .setContentText(counter.toString())
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.small_icon, "Toast Message", actionPending)
+                .addAction(R.drawable.small_icon, "Dismiss", dismissPending)
         }else{
             builder.setSmallIcon(R.drawable.small_icon)
                 .setContentTitle("Notification Title")
                 .setContentText("This is the notification")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.small_icon, "Toast Message", actionPending)
+                .addAction(R.drawable.small_icon, "Dismiss", dismissPending)
+
         }
 
         val notificationManagerCompat = NotificationManagerCompat.from(this@MainActivity)
---
+++ app/src/main/java/com/example/notification/ReceiverDismiss.kt
@@ -0,0 +1,17 @@
+package com.example.notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationManagerCompat
+
+class ReceiverDismiss: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+
+        p0?.let{
+            val notificationManagerCompat = NotificationManagerCompat.from(it)
+            notificationManagerCompat.cancel(1)
+        }
+
+    }
+}
